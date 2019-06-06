e40c1521c7beffd0e55305de949ffb94641a5aaa
pingold16 <p.ingold@outlook.com>
2019-06-06 15:39:17 -0300

    Create Triggers.sql

---
+++ Triggers.sql
@@ -0,0 +1,24 @@
+/*a.	Cuando se ingrese un movimiento de tipo Transferencia, realizar un disparador que registre dicha 
+transferencia en la tabla correspondiente, si la cuenta destino de la transferencia es una cuenta de otro 
+banco (Externa), que registre como número de cuenta 99999, como nombre de banco ‘Banco Externo’ y como estado ‘Auditoria’.*/
+
+
+/*b.	Crear un disparador que al modificarse el importe de un movimiento deje un registro en una tabla de auditoría, esta tabla debe tener la siguiente estructura:
+Auditoria(idAudit,fchAudit,idMovim,idCliente,NombreCliente,ImporteAnterior,ImporteActual)
+El campo idAudit debe ser autoincremental y fchAudit también debe registrar la hora*/
+
+
+/*c.	Mediante el uso de un disparador, no permitir ingresar un Movimiento de Salida de una cuenta que no tenga saldo.*/
+CREATE TRIGGER movimientoSalida
+ON Movimiento
+INSTEAD OF INSERT
+AS
+BEGIN
+	
+END
+
+/*d.	Mediante un disparador, no permitir crear una nueva cuenta si el cliente ya tiene una cuenta en la misma moneda y en la misma sucursal.*/
+
+
+/*e.	Implementar un disparador que controle el borrado de una sucursal, para permitir el mismo, dicho disparador debe 
+“mover” antes todas las cuentas a la sucursal más antigua del banco (obtener la sucursal más antigua de acuerdo a los movimientos).*/
